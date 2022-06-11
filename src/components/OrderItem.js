0ae45693e767e837017e9999af140d9f6a5474d1
stretyakov <stretyakov@graphpad.com>
2022-06-11 12:08:23 +0300

    [tret]

--- src/components/OrderItem.js
+++ src/components/OrderItem.js
@@ -1,30 +1,12 @@
-import React, { useState, useEffect, useContext } from "react";
-import { Card, Col, Image, Form, Button } from "react-bootstrap";
+import React, { useContext } from "react";
+import { Card, Col, Image } from "react-bootstrap";
 import { useNavigate } from "react-router-dom";
 import { PRODUCT_ROUTE } from "../utils/consts";
 import { Context } from "..";
 
 const OrderItem = ({ productId, name, price, img, count }) => {
 	const navigate = useNavigate();
-	const [newCount, setNewCount] = useState(count);
-	const { cart } = useContext(Context);
-
-	useEffect(() => {
-		let cartLS = localStorage.getItem("cart") || "[]";
-		cartLS = JSON.parse(cartLS);
-		const findInCart = cartLS.findIndex((item) => item.productId === productId);
-		cartLS[findInCart].count = +newCount;
-		localStorage.setItem("cart", JSON.stringify(cartLS));
-		cart.recalculate();
-	}, [cart, newCount, productId]);
-
-	const removeFromCart = () => {
-		let cartLS = localStorage.getItem("cart") || "[]";
-		cartLS = JSON.parse(cartLS);
-		cartLS = cartLS.filter((item) => item.productId !== productId);
-		localStorage.setItem("cart", JSON.stringify(cartLS));
-		cart.recalculate();
-	};
+	const { user } = useContext(Context);
 
 	return (
 		<Col md={3} className={"mt-3"}>
@@ -36,28 +18,8 @@ const OrderItem = ({ productId, name, price, img, count }) => {
 				<Image width={150} height={150} src={process.env.REACT_APP_DO_BUCKET_URL + img} />
 				<div>{name}</div>
 				<div>price: {price}</div>
+				<div>count: {count}</div>
 			</Card>
-			<Form>
-				<Form.Label>Count:</Form.Label>
-				<Form.Control
-					className="mt-3"
-					value={newCount}
-					onChange={(e) => {
-						setNewCount(e.target.value);
-					}}
-					type="number"
-					min="1"
-				/>
-				<Button
-					onClick={() => {
-						removeFromCart();
-					}}
-					type="button"
-					min="1"
-				>
-					X
-				</Button>
-			</Form>
 		</Col>
 	);
 };
