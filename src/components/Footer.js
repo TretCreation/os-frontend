4c9c4d1caae432eb0af70fdcb02c20d54b814bf5
Volodymyr Tretyakov <tret.creation@gmail.com>
2022-06-14 18:48:31 +0300

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (
         <div id="footer">
             <div className="container">
-                <div>Copyright © 2022</div>
+                <div>Copyright © 2022 TRET STORE</div>
             </div>
         </div>
     );
