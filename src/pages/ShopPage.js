bdd6a3302493e4e6cda131facb35426665f2b737
Volodymyr Tretyakov <tret.creation@gmail.com>
2022-06-14 20:06:58 +0300

    footer

--- src/pages/ShopPage.js
+++ src/pages/ShopPage.js
@@ -10,30 +10,36 @@ import { fetchProducts } from "../http/productAPI";
 import { toJS } from "mobx";
 
 const ShopPage = observer(() => {
-	const { shop } = useContext(Context);
+    const { shop } = useContext(Context);
 
-	useEffect(() => {
-		const brandIds = toJS(shop.filterBrands).map((item) => item.id);
-		fetchProducts(brandIds, shop.filterType.id, shop.page, 6, shop.filterText).then((data) => {
-			shop.setProducts(data.rows);
-			shop.setTotalCount(data.count);
-		});
-	}, [shop.page, shop.filterBrands, shop.filterType, shop.filterText, shop]);
+    useEffect(() => {
+        const brandIds = toJS(shop.filterBrands).map((item) => item.id);
+        fetchProducts(
+            brandIds,
+            shop.filterType.id,
+            shop.page,
+            6,
+            shop.filterText
+        ).then((data) => {
+            shop.setProducts(data.rows);
+            shop.setTotalCount(data.count);
+        });
+    }, [shop.page, shop.filterBrands, shop.filterType, shop.filterText, shop]);
 
-	return (
-		<Container>
-			<Row>
-				<Col md={3}>
-					<BrandsBar />
-				</Col>
-				<Col md={9}>
-					<TypesBar />
-					<ProductList />
-					<Pages />
-				</Col>
-			</Row>
-		</Container>
-	);
+    return (
+        <Container className="main-container">
+            <Row>
+                <Col md={3}>
+                    <BrandsBar />
+                </Col>
+                <Col md={9}>
+                    <TypesBar />
+                    <ProductList />
+                    <Pages />
+                </Col>
+            </Row>
+        </Container>
+    );
 });
 
 export default ShopPage;
